7f97b9508ab92173e4ffc540da9b91b297d610b2
Ahmad Miqdaad <ahmad.miqdaad@kalsym.com>
2023-03-28 10:24:36 +0800

    page service down

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/page-not-found'),
+      dir: require('path').join(__dirname, './coverage/page-service-down'),
       subdir: '.',
       reporters: [
         { type: 'html' },
